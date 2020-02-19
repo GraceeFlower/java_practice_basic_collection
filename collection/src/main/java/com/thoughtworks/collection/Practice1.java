643013a08ecfd5e282eb64280d5c7f72e457b7e6
GraceeFlower <1491644103@qq.com>
2020-02-19 20:31:15 +0800

    [WM] feat: done the demo

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -11,6 +13,11 @@ public class Practice1 {
      *  生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        Collection<Integer> numList = new HashSet<>();
+        Random random = new Random();
+        while (numList.size() < 10) {
+            numList.add(random.nextInt(21));
+        }
+        return numList;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,18 +1,41 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        List<Integer> numList = getNumbers(100);
+        for(int i = 0; i < numList.size(); i++) {
+            System.out.println(numList.get(i));
+        }
+
+        System.out.println("=========");
+
+        for(Integer i: numList) {
+            System.out.println(i);
+        }
+
+        System.out.println("=========");
+
+        Iterator<Integer> numIterator = numList.iterator();
+        while (numIterator.hasNext()) {
+            System.out.println(numIterator.next());
+        }
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
-    public static List<Integer> getNumbers() {
-        return null;
+    public static List<Integer> getNumbers(int num) {
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 0; i <= num; i++) {
+            numList.add(i);
+        }
+        return numList;
     }
 
 
